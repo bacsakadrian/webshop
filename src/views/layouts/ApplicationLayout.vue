7a68c671627cab5bab8c50cd24a96fad4eb661e9
Adrián <bacsakadrian@edu.bme.hu>
2021-07-07 17:01:51 +0200

    fejléc

--- src/views/layouts/ApplicationLayout.vue
+++ src/views/layouts/ApplicationLayout.vue
@@ -1,20 +1,43 @@
 <template>
-    <router-link to="/">.</router-link>
-    <router-link to="kosar">Kosár</router-link>
-    <div class="uk-container uk-container-expand"></div>
-        <div uk-grid>
-            <div class="uk-width-1-1@s uk-width-1-2@m">
+    <div class="topbar uk-container uk-container-expand">
+        <div class="uk-light">
+            <ul class="topbarlist">
+                    <li><router-link to="/">Főoldal</router-link></li>
+                    <li><router-link to="kosar">Kosár</router-link></li>
+            </ul>
+        </div>
+    </div>
+    <div class="uk-container uk-container-expand">
+        <nav style="background: white" class="uk-navbar-container" uk-navbar>
+        <div class="uk-navbar-left">
+
+            <a class="uk-navbar-item uk-logo" href="#">Logo</a>
+
+            <ul class="uk-navbar-nav">
+                <li>
+                    <a href="#">
+                        <span class="uk-icon uk-margin-small-right" uk-icon="icon: star"></span>
+                        Features
+                    </a>
+                </li>
+            </ul>
+
+            <div class="uk-navbar-item">
+                <div>Some <a href="#">Link</a></div>
             </div>
-    <div class="uk-width-1-1@s uk-width-1-2@m">
-    </div> 
-  </div>
 
-  <h1 style="font-family: Montserrat"> Montserrat </h1>
-  <h1 style="font-family: Roboto"> Roboto </h1>
+            <div class="uk-navbar-item">
+                <form action="javascript:void(0)">
+                    <input class="uk-input uk-form-width-small" type="text" placeholder="Input">
+                    <button class="uk-button uk-button-default">Button</button>
+                </form>
+            </div>
 
-  <frontpage></frontpage>
+        </div>
+    </nav>
+    <frontpage/>    
     <router-view/>
-
+    </div>
 </template>
 
 <script>
@@ -30,6 +53,20 @@
     @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap');
     @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
     @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+
+    li {
+        padding-left: 5px;
+    }
+    .topbar { 
+        color:white;
+        background-color: darkgrey
+    }
+    .topbarlist {
+        display: flex; 
+        list-style: none; 
+        margin-bottom: 0px;
+    }
+
 </style>
 
 <style lang="scss">
--- src/views/pages/Frontpage.vue
+++ src/views/pages/Frontpage.vue
@@ -1,3 +1,3 @@
 <template>
-    <h1> Kezdőlap </h1>
+    Főoldal
 </template>
